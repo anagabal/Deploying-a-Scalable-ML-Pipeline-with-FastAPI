b1ee989cb9fb8bc2990e04296b1ad78c74445f57
Ana <anagabal42@gmail.com>
2026-08-20 13:01:58 -0700

    updated files to connect to API

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-# path for saved model
+# path for saved encoder
 path = os.path.join("model", "encoder.pkl")
 encoder = load_model(path)
 
-# path for saved encoder
+# path for saved model
 path = os.path.join("model", "model.pkl")
 model = load_model(path)
 
@@ -41,20 +41,21 @@ app = FastAPI()
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    return {"greeting": "Hello! Welcome to my API :)"}
-    pass
+    return {"greeting": "Hello welcome to my API"}
 
 
 # create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
-    data_dict = data.dict()
+    ##data_dict = data.model_dump()
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
     # The data has names with hyphens and Python does not allow those as variable names.
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
-    data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
-    data = pd.DataFrame.from_dict(data)
+    ##data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
+    data_dict = data.model_dump(by_alias=True)
+
+    data = pd.DataFrame.from_dict({k: [v] for k, v in data_dict.items()})
 
     cat_features = [
         "workclass",
@@ -75,4 +76,6 @@ async def post_inference(data: Data):
 
     # code to predict the result
     _inference = inference(model, data_processed)
-    return {"result": apply_label(_inference[0])}
+    
+    return {"result": apply_label(_inference)}
+
